f4da4d74c78564843412a61517e357ea811002de
IEdiong@Mint <ediongi6@hotmail.com>
2021-09-23 04:44:40 +0100

    Digital clock fully functional

--- script.js
+++ script.js
@@ -6,12 +6,24 @@ const locale = "en-GB";
 const options = {
   second: "numeric",
   minute: "numeric",
-  hour: "numeric",
+  hour: "2-digit",
+  hourCycle: "h12",
 };
 
 // Update the UI
 setInterval(() => {
+  // Test code starts here
+  // This portion of code is for testing purposes only
+  // let consoleTime = new Intl.DateTimeFormat("en-GB", {
+  //   second: "numeric",
+  //   minute: "numeric",
+  //   hour: "2-digit",
+  //   hour12: true,
+  //   hourCycle: "h12",
+  // }).format(new Date());
+  // console.log(consoleTime);
+  // test code ends here
+
   let time = new Intl.DateTimeFormat(locale, options).format(new Date());
-  // console.log(time);
   clock.textContent = time;
 }, 1000);
